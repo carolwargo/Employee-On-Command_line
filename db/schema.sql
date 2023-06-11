8adddb61a5ae8b9ac6d5863773c64fa6b74e5b1b
Carol Wargo <Carolwargo@gmail.com>
2023-06-11 12:49:39 -0400

    removed create function

--- db/schema.sql
+++ db/schema.sql
@@ -29,23 +29,6 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
-
--- ADD additional function to update employee managers
-CREATE FUNCTION update_employee_manager(employee_id INT, manager_id INT)
-RETURNS VOID
-BEGIN
-    UPDATE employee
-    SET manager_id = manager_id
-    WHERE id = employee_id;
-END;
-
--- ADD additional function to view employees by manager
-CREATE FUNCTION get_employees_by_manager(manager_id INT)
-RETURNS TABLE (id INT, first_name VARCHAR(30), last_name VARCHAR(30))
-BEGIN
-    RETURN QUERY
-    SELECT id, first_name, last_name
-    FROM employee
-    WHERE manager_id = manager_id;
+    
 END;
 
