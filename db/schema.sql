552538eccf19d824a2b56996e02127822df7c395
Carol Wargo <Carolwargo@gmail.com>
2023-06-15 07:53:59 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -6,28 +6,28 @@ USE employee_on_command_line_db;
 
 -- CREATE department table
 CREATE TABLE department (
-  id INT PRIMARY KEY,
-  name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 -- CREATE the role table
 CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(18,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY(department_id)
+    REFERENCES department(id) 
 );
 
 -- CREATE the employee table
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT REFERENCES employee(id), 
+    FOREIGN KEY (role_id)
+    REFERENCES role (id) 
 );
-    
 
